f48b3688be339ec8e5ec641a675e659f9e203a52
Disha K <dishakumble08@gmail.com>
2026-02-02 22:47:21 +0530

    Refactor coffee machine functions and add profit tracking

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from data import MENU , resource
+profit = 0
 
-def print_report():
-print("\n--- Coffee Machine Report ---")
-print(f"Water: {resources['water']}ml")
-print(f"Milk: {resources['milk']}ml")
-print(f"Coffee: {resources['coffee']}g")
-print(f"Money: ${resources['money']}")
+def is_resource_sufficient(order_ingredients):
+    for item in order_ingredients:
+        if order_ingredients[item] > resources[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+    return True
+
+def process_coins():
+    total = int(input("quarters? ")) * 0.25
+    total += int(input("dimes? ")) * 0.10
+    total += int(input("nickles? ")) * 0.05
+    total += int(input("pennies? ")) * 0.01
+    return total
 
 
-def check_resources(drink):
-  for item in MENU[drink]["ingredients"]:
-    if MENU[drink]["ingredients"][item] > resources[item]:
-      print(f"Sorry there is not enough {item}.")
-      return False
-  return True
+def is_transaction_successful(money_received, drink_cost):
+    global profit
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
