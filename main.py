bab4e8cc1683efdef52f2e010fe9d4d198638a0a
Disha K <dishakumble08@gmail.com>
2026-02-05 22:01:15 +0530

    Implement coffee machine user interaction loop

--- main.py
+++ main.py
@@ -32,3 +32,25 @@ def make_coffee(drink_name, ingredients):
     for item in ingredients:
         resources[item] -= ingredients[item]
     print(f"Here is your {drink_name} ")
+
+is_on = True
+while is_on:
+    user_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+    if user_choice == "off":
+        is_on = False
+
+    elif user_choice == "report":
+        print(resources)
+        print(f"Money: ${profit}")
+
+    elif user_choice in MENU:
+        drink = MENU[user_choice]
+        if is_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(user_choice, drink["ingredients"])
+    else:
+        print("Invalid choice. Please select espresso,latte,cappuccino.")
+
+
